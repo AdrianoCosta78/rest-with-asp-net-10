d010d7da386be91c732d2c08beffd555fd6e1ce1
Adriano Costa <adriano.c-basis@ipen.br>
2026-06-08 16:07:36 -0300

    Desenvolvimeto do controller, model e service

---
+++ RestWithASPNET10/Controllers/GreetingController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestWithASPNET10.Model;
+
+
+namespace RestWithASPNET10.Controllers{
+   
+        [ApiController]
+        [Route("[controller]")]
+        public class GreetingController : ControllerBase
+        {
+            private static long counter = 0;
+            private static readonly string template = "Hello, {0}!";
+
+
+            [HttpGet]
+            public Greeting Get([FromQuery] string name = "world")
+            {
+                var id = Interlocked.Increment(ref counter);
+                var content = string.Format(template, name);
+                return new Greeting(1,content);
+            }
+        }
+    
+}
---
+++ RestWithASPNET10/Model/Greeting.cs
@@ -0,0 +1,6 @@
+﻿namespace RestWithASPNET10.Model
+{
+    public class Greeting(long Id, string Content);
+    
+    
+}
